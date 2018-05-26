4f29824be9150b98a0ee9a0f8e9b29809282d2ba
ninasjoberg <nina.sjoberg@gmail.com>
2018-05-26 17:36:01 +0200

    optimized firebase import

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,19 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBbwxIHMuLDCrqaTfrcGUGmMASYtjgdPw4",
-    authDomain: "simple-chat-3460f.firebaseapp.com",
-    databaseURL: "https://simple-chat-3460f.firebaseio.com",
-    projectId: "simple-chat-3460f",
-    storageBucket: "simple-chat-3460f.appspot.com",
-    messagingSenderId: "666229101388"
+	apiKey: "AIzaSyBbwxIHMuLDCrqaTfrcGUGmMASYtjgdPw4",
+	authDomain: "simple-chat-3460f.firebaseapp.com",
+	databaseURL: "https://simple-chat-3460f.firebaseio.com",
+	projectId: "simple-chat-3460f",
+	storageBucket: "simple-chat-3460f.appspot.com",
+	messagingSenderId: "666229101388"
 };
 firebase.initializeApp(config);
 
-  export default firebase;
+export default firebase;
 
 
 
